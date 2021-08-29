72c1f62783ad0dc6aab3616fcf226b02214221db
bel-sndct <79698612+bel-sndct@users.noreply.github.com>
2021-08-29 15:48:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsArr = bracketsConfig.map(function(item) {
+        return item.join('');
+    });
+    let res_str = str;
+    for (let i = 0; i < bracketsArr.length; i++) {
+        if (res_str.includes(bracketsArr[i])) {
+            res_str = res_str.replace(bracketsArr[i], '');
+            i = -1;
+        }
+    }
+    return res_str.length === 0;
 }
